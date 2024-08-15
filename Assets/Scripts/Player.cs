07d0e09ffdec37bc675b45a4a74114a9b9acc55a
BerkayBabaoglu <112875768+BerkayBabaoglu@users.noreply.github.com>
2024-08-15 20:11:46 +0300

    updated

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    private Rigidbody2D _rigid;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigid = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        _rigid.velocity = new Vector2(horizontalInput, _rigid.velocity.y);
     }
 }
