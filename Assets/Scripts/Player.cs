aabe3e92f7e7d021d83bd60bfa6e025fc5a7eba3
BerkayBabaoglu <112875768+BerkayBabaoglu@users.noreply.github.com>
2024-08-19 13:59:42 +0300

    updated

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,14 +9,18 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _jumpForce = 5.0f;
     private bool _resetJump = false;
+    private bool _grounded = false;
     [SerializeField]
     private float speed = 2.5f;
+    private PlayerAnimations _anim;
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
         _rigid = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<PlayerAnimations>();
     }
 
     // Update is called once per frame
@@ -28,28 +32,51 @@ public class Player : MonoBehaviour
     void Movement()
     {
         float move = Input.GetAxisRaw("Horizontal");
-        _rigid.velocity = new Vector2(move * speed, _rigid.velocity.y);
+        _grounded = IsGrounded();
 
-        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded() == true)
+        if (move > 0)
+        {
+            // Sað yöne hareket ediyor
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (move < 0)
+        {
+            // Sol yöne hareket ediyor
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded() == true)
         {
             _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
             StartCoroutine(resetJumpRoutine());
+            _anim.Jump(true);
         }
+        
+        _rigid.velocity = new Vector2(move * speed, _rigid.velocity.y);
+
+        _anim.Move(move);
     }
 
     bool IsGrounded()
     {
-        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, 1 << 8);
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 1f, 1 << 8);
+        Debug.DrawRay(transform.position, Vector2.down, Color.green);
 
         if(hitInfo.collider != null)
         {
             if(_resetJump == false)
             {
+                _anim.Jump(false);
                 return true;
             }
         }
         return false;
     }
+
+    
+    
+    
 
     IEnumerator resetJumpRoutine()
     {
