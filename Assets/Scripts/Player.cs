849e5c94cd6b0e09a03c1ab72d65bac9201e3e92
BerkayBabaoglu <112875768+BerkayBabaoglu@users.noreply.github.com>
2024-08-19 10:21:15 +0300

    updated

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,14 @@ public class Player : MonoBehaviour
 {
 
     private Rigidbody2D _rigid;
+    [SerializeField]
+    private float _jumpForce = 5.0f;
+    [SerializeField]
+    private bool _grounded = false;
+    [SerializeField]
+    private LayerMask _groundLayer;
+    private bool resetJumpNeeded = false;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +24,32 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        _rigid.velocity = new Vector2(horizontalInput, _rigid.velocity.y);
+        float move = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && _grounded == true)
+        {
+            _rigid.velocity = new Vector2(transform.position.x, _jumpForce);
+            _grounded = false;
+            resetJumpNeeded = true;
+            StartCoroutine(resetJumpNeededCoroutine());
+
+        }
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.53f, 1 << 8);
+        Debug.DrawRay(transform.position, Vector2.down * 0.53f, Color.green);
+
+        if(hitInfo.collider != null)
+        {
+            Debug.Log("Hit: " + hitInfo.collider.name);
+            if(resetJumpNeeded = false)
+                _grounded = true;
+        }
+        _rigid.velocity = new Vector2(move, _rigid.velocity.y);
+    }
+
+    IEnumerator resetJumpNeededCoroutine()
+    {
+        yield return new WaitForSeconds(0.1f);
+        resetJumpNeeded = false;
     }
 }
