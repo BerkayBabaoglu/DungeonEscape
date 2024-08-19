3cdc8a3d60919c9a9916e87c812ca1d336ed23a5
BerkayBabaoglu <112875768+BerkayBabaoglu@users.noreply.github.com>
2024-08-19 10:55:05 +0300

    jump system clean code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,11 +8,7 @@ public class Player : MonoBehaviour
     private Rigidbody2D _rigid;
     [SerializeField]
     private float _jumpForce = 5.0f;
-    [SerializeField]
-    private bool _grounded = false;
-    [SerializeField]
-    private LayerMask _groundLayer;
-    private bool resetJumpNeeded = false;
+    private bool _resetJump = false;
 
 
     // Start is called before the first frame update
@@ -24,32 +20,39 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Movement();
+    }
+
+    void Movement()
+    {
         float move = Input.GetAxisRaw("Horizontal");
+        _rigid.velocity = new Vector2(move, _rigid.velocity.y);
 
-        if (Input.GetKeyDown(KeyCode.Space) && _grounded == true)
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded() == true)
         {
-            _rigid.velocity = new Vector2(transform.position.x, _jumpForce);
-            _grounded = false;
-            resetJumpNeeded = true;
-            StartCoroutine(resetJumpNeededCoroutine());
-
+            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
+            StartCoroutine(resetJumpRoutine());
         }
+    }
 
+    bool IsGrounded()
+    {
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, 1 << 8);
-        Debug.DrawRay(transform.position, Vector2.down * 0.6f, Color.green);
 
         if(hitInfo.collider != null)
         {
-            Debug.Log("Hit: " + hitInfo.collider.name);
-            if(resetJumpNeeded == false)
-                _grounded = true;
+            if(_resetJump == false)
+            {
+                return true;
+            }
         }
-        _rigid.velocity = new Vector2(move, _rigid.velocity.y);
+        return false;
     }
 
-    IEnumerator resetJumpNeededCoroutine()
+    IEnumerator resetJumpRoutine()
     {
+        _resetJump = true;
         yield return new WaitForSeconds(0.1f);
-        resetJumpNeeded = false;
+        _resetJump = false;
     }
 }
