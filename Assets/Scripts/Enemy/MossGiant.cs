6d356ab72ebed7824aded919c798cf61118fa973
BerkayBabaoglu <112875768+BerkayBabaoglu@users.noreply.github.com>
2024-08-20 14:14:55 +0300

    updated

--- Assets/Scripts/Enemy/MossGiant.cs
+++ Assets/Scripts/Enemy/MossGiant.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class MossGiant : Enemy
 {
+    private Transform targetPoint;
+    private SpriteRenderer mossGiantSprite;
+    private bool isFacingRight = true;
+
+    
     
     void Start()
     {
-        
+        targetPoint = pointA;
+        speed = 1;
+        mossGiantSprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     public override void Update()
     {
-        Debug.Log("MossGiant is coming.");
+        transform.position = Vector2.MoveTowards(transform.position, targetPoint.position, speed * Time.deltaTime);
+
+
+        if(Vector2.Distance(transform.position, targetPoint.position) < 0.1f)
+        {
+            targetPoint = targetPoint == pointA ? pointB : pointA;
+            //yonu guncelle
+            UpdateFacingDirection();
+        }
     }
+    
+    private void UpdateFacingDirection()
+    {
+        //yonu hedef noktasina göre guncelle
+        if(targetPoint == pointA)
+        {
+            mossGiantSprite.flipX = true;
+            isFacingRight = false;
+        }
+        else if(targetPoint == pointB)
+        {
+            mossGiantSprite.flipX = false;
+            isFacingRight = true;
+        }
+    }
+
+    
     
 }
