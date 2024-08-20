860904b40475b6217083ac445bae80adb72ec564
BerkayBabaoglu <112875768+BerkayBabaoglu@users.noreply.github.com>
2024-08-20 14:53:14 +0300

    enemy AI updated

--- Assets/Scripts/Enemy/MossGiant.cs
+++ Assets/Scripts/Enemy/MossGiant.cs
@@ -7,7 +7,8 @@ public class MossGiant : Enemy
     private Transform targetPoint;
     private SpriteRenderer mossGiantSprite;
     private bool isFacingRight = true;
-
+    private Animator _anim;
+    private bool isWaiting = true;
     
     
     void Start()
@@ -15,21 +16,30 @@ public class MossGiant : Enemy
         targetPoint = pointA;
         speed = 1;
         mossGiantSprite = GetComponentInChildren<SpriteRenderer>();
+        _anim = GetComponentInChildren<Animator>();
     }
 
     public override void Update()
     {
+        if(isWaiting == true)
+            StartCoroutine(WaitForIdleAnimation());
+        else
+            Movement();
+    }
+    
+    private void Movement()
+    {
         transform.position = Vector2.MoveTowards(transform.position, targetPoint.position, speed * Time.deltaTime);
 
 
-        if(Vector2.Distance(transform.position, targetPoint.position) < 0.1f)
+        if (Vector2.Distance(transform.position, targetPoint.position) < 0.1f)
         {
             targetPoint = targetPoint == pointA ? pointB : pointA;
             //yonu guncelle
             UpdateFacingDirection();
         }
     }
-    
+
     private void UpdateFacingDirection()
     {
         //yonu hedef noktasina göre guncelle
@@ -45,6 +55,13 @@ public class MossGiant : Enemy
         }
     }
 
-    
+    IEnumerator WaitForIdleAnimation()
+    {
+        AnimatorStateInfo stateInfo = _anim.GetCurrentAnimatorStateInfo(0);
+        float idleDuration = stateInfo.length - stateInfo.normalizedTime * stateInfo.length;
+
+        yield return new WaitForSeconds(idleDuration);
+        isWaiting = false;
+    }
     
 }
